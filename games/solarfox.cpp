279625d9ac5c316c4f4dce7d2d4017bee360ced1
Le_Blank0 <lucas.goetz@epitech.eu>
2020-04-10 11:51:27 +0200

    Start SolarFox

--- games/solarfox.cpp
+++ games/solarfox.cpp
@@ -7,25 +7,73 @@
 
 Solarfox::Solarfox()
 {
+    // remplace la var map par la lecture de fichier dans assets (fait gaffe le chemin c'est juste ./assets/SolarFoxMap1)
+    int fd = open("/dev/urandom", O_RDONLY);
+    unsigned seed;
+
+    read(fd, &seed, sizeof seed);
+    srand(seed);
+    for (int i = 0; i < 20; i++) {
+        std::vector<Rect> tmp;
+        rect.push_back(tmp);
+        for (int y = 0; y < 20; y++) {
+            Rect rtmp = Rect(Vector2f(i * 5, y * 5), Vector2f(40, 30), colorBlack);
+            rect[i].push_back(rtmp);
+        }
+    }
 }
 
 void Solarfox::handleEvent(const std::string &event)
 {
+    if (event == "right" && move == false) {
+        dir += 1;
+        move = true;
+    }
+    if (event == "left" && move == false) {
+        dir -= 1;
+        move = true;
+    }
+    if (event == "space" && speed == 150000) {
+        speed = 120000;
+    } else if (event == "space" && speed == 120000)
+        speed = 150000;
+    if (dir < 0)
+        dir = 3;
+    if (dir > 3)
+        dir = 0;
 }
 
 void Solarfox::handleUpdate(int t)
 {
+    if (t - timeShip > speed) {
+        move = false;
+        moveShip();
+        collision();
+        for (int i = 0; i < 20; i++)
+            for (int y = 0; y < 20; y++) {
+                handleCoins(i, y);
+                setMapRectColor(i, y);
+            }
+        timeShip = t;
+    }
+    if (t - timeEnnemies > 150000) {
+        timeEnnemies = t;
+    }
+    if (t - timeShoot > 50000) {
+        timeShoot = t;
+    }
 }
 
 void Solarfox::handleRender(IGraphicRenderer &renderer) const
 {
     renderer.clearScreen();
-    renderer.drawRect(Rect(Vector2f(10,10), Vector2f(250,250), Color(0,255,0,255)));
-    //    for (int i = 0; i < 20; i++)
-//        for (int y = 0; y < 20; y++) {
-//            renderer.drawRect(rect[i][y]);
-//        }
-    //    renderer.drawText(Text("NIK STOP", Vector2f(10,10), Vector2f(10, 10), Color(255, 255, 255, 0)));
+    for (int i = 0; i < 20; i++)
+        for (int y = 0; y < 20; y++) {
+            renderer.drawRect(rect[i][y]);
+        }
+    renderer.drawText(name);
+    renderer.drawText(scoretxt);
+    renderer.drawText(scoreNb);
     renderer.drawScreen();
 }
 
@@ -36,6 +84,40 @@ const std::map<std::string, std::string> Solarfox::getGameData()
     if (_gameOver)
         map["go"] = "yes";
     return (map);
+}
+
+void Solarfox::handleCoins(int i, int y) {
+    if (map[i][y] == '1' && ship.x == rect[y][i].getPositionX() && ship.y == rect[y][i].getPositionY()) {
+        score++;
+        map[i][y] = '0';
+        scoreNb.setText(std::to_string(score));
+    }
+}
+
+void Solarfox::moveShip() {
+    if (dir == 0)
+        ship -= Vector2f(0, 5);
+    if (dir == 1)
+        ship += Vector2f(5, 0);
+    if (dir == 2)
+        ship += Vector2f(0, 5);
+    if (dir == 3)
+        ship -= Vector2f(5, 0);
+}
+
+void Solarfox::collision() {
+    if (!(ship.x >= 0 && ship.x <= 95 && ship.y >= 10 && ship.y <= 85))
+        _gameOver = true;
+}
+
+void Solarfox::setMapRectColor(int i, int y) {
+    rect[y][i].setColor(colorBlack);
+    if (map[i][y] == '*')
+        rect[y][i].setColor(colorBorder);
+    if (map[i][y] == '1')
+        rect[y][i].setColor(colorCoins);
+    if (ship.x == rect[y][i].getPositionX() && ship.y ==        rect[y][i].getPositionY())
+        rect[y][i].setColor(colorShip);
 }
 
 extern "C"
--- include/solarfox.hpp
+++ include/solarfox.hpp
@@ -2,8 +2,8 @@
 // Created by blanko on 01/04/2020.
 //
 
-#ifndef OOP_ARCADE_2019_SNAKE_HPP
-#define OOP_ARCADE_2019_SNAKE_HPP
+#ifndef OOP_ARCADE_2019_SOLARFOX_HPP
+#define OOP_ARCADE_2019_SOLARFOX_HPP
 
 #include "interface/IGame.hpp"
 #include <vector>
@@ -21,29 +21,55 @@ public:
     void handleRender(IGraphicRenderer &renderer) const override;
     const std::map<std::string, std::string> getGameData() override;
 
+    void moveShip();
+    void collision();
+    void handleCoins(int i, int y);
+    void setMapRectColor(int i, int y);
+
 private:
     Color colorBlack = Color(0, 0, 0, 255);
-    Color colorMap = Color(255, 0, 255, 255);
-    Color colorBorder = Color(255, 0, 125, 255);
-    Color colorSnake = Color(61, 154, 59, 255);
-    Color colorApple = Color(255, 125, 0, 255);
+    Color colorBorder = Color(50, 50, 50, 255);
+    Color colorShip = Color(61, 154, 59, 255);
+    Color colorCoins = Color(255, 125, 0, 255);
+    Color colorShoot = Color(255, 0, 255, 255);
 
     bool _gameOver = false;
-    int size_snake = 4;
-    int eat = 0;
-    int time = 0;
+    int score = 0;
+    int speed = 150000;
+    int timeShip = 0;
+    int timeEnnemies = 0;
+    int timeShoot = 0;
     int dir = 0;
     bool move = false;
-    Vector2f apple = Vector2f(15, 70);
 
-    std::vector<std::vector<Rect>> rect;
-    std::vector<Vector2f> snake = {
-        Vector2f(50, 50),
-        Vector2f(50, 55),
-        Vector2f(50, 60),
-        Vector2f(50, 65),
+    std::vector<std::string> map = {
+        "********************",
+        "********************",
+        "00000000000000000000",
+        "00000011011011000000",
+        "00000100100100100000",
+        "00000011011011000000",
+        "00000000100100000000",
+        "00000000011000000000",
+        "00000000100100000000",
+        "00000011011011000000",
+        "00000100100100100000",
+        "00000011011011000000",
+        "00000000100100000000",
+        "00000000011000000000",
+        "00000000100100000000",
+        "00000011011011000000",
+        "00000100100100100000",
+        "00000000000000000000",
+        "********************",
+        "********************",
     };
+    std::vector<std::vector<Rect>> rect;
+    Vector2f ship = Vector2f(50, 50);
+    Text name = Text(" SolarFox ", Vector2f(2, -1), Vector2f(5, 5), colorBlack);
+    Text scoretxt = Text(" Score : ", Vector2f(40, -1), Vector2f(5, 5), colorBlack);
+    Text scoreNb = Text("0", Vector2f(55, -1), Vector2f(5, 5), colorBlack);
 };
 
 
-#endif //OOP_ARCADE_2019_SNAKE_HPP
+#endif //OOP_ARCADE_2019_SOLARFOX_HPP
